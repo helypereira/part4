14002ef9ed6df2c4655c17346c7a4d9bfad1424f
helypereira <almeidahely@gmail.com>
2025-08-28 16:24:53 -0400

    4.2 Blog List, step 2

--- 4.1/controllers/blogs.js
+++ 4.1/controllers/blogs.js
@@ -34,3 +34,32 @@ export const createBlog = async (req, res, next) => {
         next(error)
     }
 }
+
+export const updateBlog = async (req, res, next) => {
+    try {
+        const {title, author, url, likes} = req.body
+        const blog = await Blog.findByIdAndUpdate(
+            req.params.id, {
+                title, author, url, likes
+            },
+            {
+                new: true,
+                runValidators: true
+            }
+        )
+        
+        updateBlog ? res.json(updateBlog): res.status(404).json({error: 'Blog no found.'})
+    } catch (error) {
+        next(error)
+    }
+}
+
+
+export const deleteBlog = async(req, res, next) => {
+    try {
+        const deleteBlog = await Blog.findByIdAndDelete(req.params.id)
+        deleteBlog ? res.status(204).end() : res.status(404).json({error: 'Blog no found.'})
+    } catch (error) {
+        next(error)
+    }
+}
--- 4.1/routes/blogs.js
+++ 4.1/routes/blogs.js
@@ -3,7 +3,9 @@ import express from 'express'
 import {
     getAllBlogs,
     getBlogByID,
-    createBlog
+    createBlog,
+    updateBlog,
+    deleteBlog
 } from '../controllers/blogs.js'
 
 const router = express.Router()
@@ -11,5 +13,7 @@ const router = express.Router()
 router.get('/', getAllBlogs)
 router.get('/:id', getBlogByID)
 router.post('/',createBlog)
+router.put('/:id', updateBlog)
+router.delete('/:id', deleteBlog)
 
 export default router;
